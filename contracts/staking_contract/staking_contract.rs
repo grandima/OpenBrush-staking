73d7fd46917a731d9bbd84a6029461ac97c30793
Dmytro Medynskyi <grandima@gmail.com>
2023-03-29 14:38:19 +0200

    create staking_contract

--- contracts/staking_contract/staking_contract.rs
+++ contracts/staking_contract/staking_contract.rs
@@ -10,32 +10,23 @@ mod staking_token {
         }
     };
 
-    impl PSP22 for StakingToken {}
-    
-    impl PSP22Metadata for StakingToken {}
+    const DAY_SECONDS: u128 = 60 * 60 * 24;
 
     #[ink(storage)]
     #[derive(Storage, Default)]
-    pub struct StakingToken {
-        #[storage_field]
-        psp22: psp22::Data,
-        #[storage_field]
-        metadata: metadata::Data
+    pub struct StakingContract {
+        staking_token: AccountId,
+        mapping: Mapping<AccountId, ()>
     }
 
-    impl StakingToken {
+    impl StakingContract {
         #[ink(constructor)]
-        pub fn new(name: Option<String>, symbol: Option<String>, staking_account: AccountId) -> Self {
-            let mut contract = Self::default();
-            contract.metadata.name = name;
-            contract.metadata.symbol = symbol;
-            let decimals: u8 = 18;
-            contract.metadata.decimals = decimals;
-            let initial_supply = 1_000_000_000 * (10 as u128).pow(decimals as u32);
-            contract.psp22.supply = initial_supply;
-            assert!(contract._mint_to(staking_account, initial_supply * 70 / 100).is_ok());
-            assert!(contract._mint_to(Self::env().caller(), initial_supply * 30 / 100).is_ok());
-            contract
+        pub fn new(staking_token: AccountId) -> Self {
+            Self {staking_token: staking_token}
+        }
+        #[ink(message)]
+        pub fn stake(&self, amount: Balance) {
+
         }
     }
 }
