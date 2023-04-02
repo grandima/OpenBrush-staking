039964a71b1920c19ef20c3f1369c2dcb04ea3ab
Dmytro Medynskyi <grandima@gmail.com>
2023-04-02 22:18:19 +0200

    change reward logic

--- contracts/staking_contract/staking_contract.rs
+++ contracts/staking_contract/staking_contract.rs
@@ -4,7 +4,8 @@ mod staking_contract {
     use openbrush::{
         contracts::{
             psp22::extensions::metadata::*,
-            traits::psp22::PSP22Ref
+            traits::psp22::PSP22Ref,
+            psp37::extensions::{burnable::*, mintable::*},
         },
         traits::{
             Storage
@@ -17,13 +18,11 @@ mod staking_contract {
     #[cfg_attr(feature = "std", derive(StorageLayout, scale_info::TypeInfo))]
     struct Record {
         pub staked_amount: Balance,
-        pub share_amount: Balance,
         pub staked_at: Timestamp,
-        pub last_claimed: Timestamp,
     }
     impl Default for Record {
         fn default() -> Self {
-            Self {staked_amount: 0, share_amount: 0, staked_at: 0, last_claimed: 0}
+            Self {staked_amount: 0, staked_at: 0}
         }
     }
 
@@ -37,19 +36,25 @@ mod staking_contract {
         PSP22Error(PSP22Error)
     }
 
+    impl PSP37 for StakingContract {}
+
     #[ink(storage)]
     #[derive(Storage)]
     pub struct StakingContract {
         token_addr: AccountId,
         stake_info: Mapping<AccountId, Record>,
         total_staked_amount: Balance,
-        available_reward_amount: Balance
+        //for simplicity it's 700_000_000
+        available_reward_amount: Balance,
+        initial_timestamp: Timestamp,
+        #[storage_field]
+        psp37: psp37::Data,
     }
 
     impl StakingContract {
         #[ink(constructor)]
         pub fn new(token: AccountId, available_reward_amount: Balance) -> Self {
-            Self {token_addr: token, stake_info: Default::default(), total_staked_amount: 0, available_reward_amount: available_reward_amount}
+            Self {token_addr: token, stake_info: Default::default(), total_staked_amount: 0, available_reward_amount: available_reward_amount, initial_timestamp: Self::env().block_timestamp(), psp37: Default::default()}
         }
         #[ink(message)]
         pub fn stake(&mut self, amount: Balance) -> Result<(), Error> {
@@ -77,7 +82,7 @@ mod staking_contract {
             };
             self.total_staked_amount += final_staked_amount;
             let block_timestamp = self.env().block_timestamp();
-            let record = Record {staked_amount: final_staked_amount, share_amount: 0, staked_at:  block_timestamp, last_claimed: 0};
+            let record = Record {staked_amount: final_staked_amount, staked_at:  block_timestamp};
             self.stake_info.insert(&from, &record);
             return Ok(());
         }
@@ -105,37 +110,40 @@ mod staking_contract {
                 return Err(Error::NoRecord(account_id))
             };
             let block_timestamp = self.env().block_timestamp();
-            let staked_at = record.staked_at;
-            let reward_amount = self._calculate_reward(block_timestamp, staked_at, record.staked_amount) - self._calculate_reward(block_timestamp, record.last_claimed, record.staked_amount);
+            let start_staking_timestamp = record.staked_at;
+            let initial_timestamp = self.initial_timestamp;
+            let reward_amount = self._calculate_reward(block_timestamp, initial_timestamp, start_staking_timestamp, self.available_reward_amount / self.total_staked_amount * record.staked_amount);
             if reward_amount == 0 {
                 return Ok(());
             }
-
             //ERRR: check transfer error
             let _ = PSP22Ref::transfer_from(&self.token_addr, self.env().account_id(), self.env().caller(), reward_amount, Vec::<u8>::new());
-            self.available_reward_amount -= reward_amount;
-            record.last_claimed = block_timestamp;
+            record.staked_at = block_timestamp;
             self.stake_info.insert(&account_id, &record);
             Ok(())
         }
 
-        fn _calculate_reward(&self, block_timestamp: Timestamp, staked_at: Timestamp, share: Balance) -> Balance {
-            if staked_at < block_timestamp {
+        fn _calculate_reward(&self, block_timestamp: Timestamp, initial_timestamp: Timestamp, start_staking_timestamp: Timestamp, share: Balance) -> Balance {
+            if initial_timestamp < block_timestamp {
                 return 0;
             }
-            let year: u128 = 365;
+            let year = 365;
             let day = 24 * 60 * 60;
-            let staking_days = (block_timestamp - staked_at) as u128 / (24 * 60 * 60);
-            let mut full_staking_years = staking_days / year;
+            let era = (block_timestamp - initial_timestamp) / day / year;
+            let era_start_timestamp = era * year * day;
+            let mut start_staking_timestamp = start_staking_timestamp;
+            if era_start_timestamp > start_staking_timestamp {
+                start_staking_timestamp = era_start_timestamp;
+            }
+            let staking_days = (block_timestamp - era_start_timestamp) / day;
+            let mut full_staking_years = era;
             let left_staking_days = staking_days - full_staking_years * day;
-            let full_share = share;
-            let mut total_reward = full_share;
+            let mut total_reward = share;
             while full_staking_years != 0 {
                 total_reward /= 2;
                 full_staking_years -= 1;
             }
-            let days_share = (total_reward / 2) * left_staking_days / year;
-            total_reward = full_share - total_reward + days_share;
+            total_reward = (total_reward / 2) * (((block_timestamp - start_staking_timestamp) / day) / year) as u128;
             return total_reward;
         }
 
@@ -146,9 +154,5 @@ mod staking_contract {
             self.available_reward_amount >= amount
         }
     }
-    // impl Into<Error> for PSP22Error {
-    //     fn into(self) -> Error {
-    //         Error::PSP22Error(self)
-    //     }
-    // }
+
 }
