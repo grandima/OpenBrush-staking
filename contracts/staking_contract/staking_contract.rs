efdfe3341664418d819a480dc80d88f6ef34e4d2
Dmytro Medynskyi <grandima@gmail.com>
2023-04-03 00:27:21 +0200

    implement reputation claming

--- contracts/staking_contract/staking_contract.rs
+++ contracts/staking_contract/staking_contract.rs
@@ -5,25 +5,35 @@ mod staking_contract {
         contracts::{
             psp22::extensions::metadata::*,
             traits::psp22::PSP22Ref,
-            psp37::extensions::{burnable::*, mintable::*},
+            psp37::Internal,
+            psp37::extensions::{mintable::*},
         },
         traits::{
             Storage
         },
         storage::Mapping
     };
-
-    use ink::storage::traits::StorageLayout;
+    use ink::{
+        storage::traits::StorageLayout,
+    };
+    const DAY_SECONDS: u64 = 60 * 60 * 24;
+    const YEAR: u64 = 365;
     #[derive(Debug, Clone, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(StorageLayout, scale_info::TypeInfo))]
     struct Record {
         pub staked_amount: Balance,
         pub initial_timestamp: Timestamp,
         pub staked_at: Timestamp,
+        pub last_reputation_update: Timestamp
     }
     impl Default for Record {
         fn default() -> Self {
-            Self {staked_amount: 0, initial_timestamp: 0, staked_at: 0}
+            Self {staked_amount: 0, initial_timestamp: 0, staked_at: 0, last_reputation_update: 0}
+        }
+    }
+    impl Record {
+        pub fn calculate_reputation(&self, block_timestamp: Timestamp) -> u128 {
+            ((block_timestamp - self.last_reputation_update) / DAY_SECONDS) as u128 * self.staked_amount
         }
     }
 
@@ -50,12 +60,13 @@ mod staking_contract {
         initial_timestamp: Timestamp,
         #[storage_field]
         psp37: psp37::Data,
+        reputation_map: Mapping<AccountId, u128>,
     }
 
     impl StakingContract {
         #[ink(constructor)]
         pub fn new(token: AccountId, available_reward_amount: Balance) -> Self {
-            Self {token_addr: token, stake_info: Default::default(), total_staked_amount: 0, available_reward_amount: available_reward_amount, initial_timestamp: Self::env().block_timestamp(), psp37: Default::default()}
+            Self {token_addr: token, stake_info: Default::default(), total_staked_amount: 0, available_reward_amount: available_reward_amount, initial_timestamp: Self::env().block_timestamp(), psp37: Default::default(), reputation_map: Default::default()}
         }
         #[ink(message)]
         pub fn stake(&mut self, amount: Balance) -> Result<(), Error> {
@@ -80,7 +91,7 @@ mod staking_contract {
             };
             self.total_staked_amount += final_staked_amount;
             let block_timestamp = self.env().block_timestamp();
-            let record = Record {staked_amount: final_staked_amount, initial_timestamp: block_timestamp, staked_at:  block_timestamp};
+            let record = Record {staked_amount: final_staked_amount, initial_timestamp: block_timestamp, staked_at:  block_timestamp, last_reputation_update: block_timestamp};
             self.stake_info.insert(&from, &record);
             return Ok(());
         }
@@ -125,8 +136,8 @@ mod staking_contract {
             if initial_timestamp < block_timestamp {
                 return 0;
             }
-            let year = 365;
-            let day = 24 * 60 * 60;
+            let year = YEAR;
+            let day = DAY_SECONDS;
             let era = (block_timestamp - initial_timestamp) / day / year;
             let era_start_timestamp = era * year * day;
             let mut start_staking_timestamp = start_staking_timestamp;
@@ -143,8 +154,37 @@ mod staking_contract {
             return total_reward;
         }
 
-        fn _get_record(&self, account_id: AccountId) -> Record {
-            self.stake_info.get(&account_id).unwrap_or(Record::default())
+
+        pub fn claim_reputation(&mut self, account_id: AccountId) {
+            let pending_rep = match self.stake_info.get(&account_id) {
+                Some(mut record) => {
+                    let block_timestamp = self.env().block_timestamp();
+                    let new_rep = record.calculate_reputation(block_timestamp);
+                    record.last_reputation_update = block_timestamp;
+                    self.stake_info.insert(&account_id, &record);
+                    new_rep
+                },
+                None => 0
+            };
+            let old_rep = self.reputation_map.get(&account_id).unwrap_or(0);
+            let new_rep = old_rep + pending_rep;
+            //ERRR
+            if old_rep == new_rep  {
+                return;
+            }
+            self.reputation_map.insert(&account_id, &new_rep);
+            let mut rep_billions = new_rep / 1_000_000_000_u128;
+            let mut level = 0;
+            while rep_billions != 0 {
+                rep_billions /= 10_u128;
+                level += 1;
+            }
+            if self.balance_of(account_id, Some(Id::U128(level))) == 0 {
+                let mut out_vec = Vec::new();
+                out_vec.push((Id::U128(level), 1));
+                //ERRR
+                let _ = self._mint_to(account_id, out_vec);
+            }
         }
     }
 
